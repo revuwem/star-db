1588ab2159d3e16f02c0e346a09697ae40f07d07
revuwem <revuwem@gmail.com>
2020-09-02 13:00:04 +0300

    created randomplanet component

---
+++ src/components/random-planet/random-planet.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './random-planet.css';
+
+const RandomPlanet = () => {
+    return (
+        <div className="random-planet jumbotron rounded">
+            <img className="planet-image"
+                src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+            <div>
+                <h4>Earth</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>1500390</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>1500</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>5680040</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default RandomPlanet;
