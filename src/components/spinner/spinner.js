5501b8be72729af7d273befeb4b0c732b50a6957
revuwem <revuwem@gmail.com>
2020-09-02 15:40:58 +0300

    added mx-auto classname

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -4,7 +4,7 @@ import './spinner.css';
 
 const Spinner = () => {
     return (
-        <div class="loadingio-spinner-rolling-yhtmtjl5pn">
+        <div class="loadingio-spinner-rolling-yhtmtjl5pn mx-auto">
             <div class="ldio-c2ut2dmmi3">
                 <div></div>
             </div>
