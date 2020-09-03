732b41d1276e40b23ccb8a8598cc2f5ae5f05a16
revuwem <revuwem@gmail.com>
2020-09-03 15:28:28 +0300

    created base people page

---
+++ src/components/people-page/people-page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './people-page.css';
+
+export default class PeoplePage extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+            <p>People Page</p>
+        );
+    }
+}
