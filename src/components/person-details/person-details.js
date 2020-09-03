ade2a2f59e48657d0fa6e1d202da85247ed9c994
revuwem <revuwem@gmail.com>
2020-09-03 12:53:43 +0300

    refactored to a class component

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -2,31 +2,40 @@ import React from 'react';
 
 import './person-details.css';
 
-const PersonDetails = () => {
-    return (
-        <div className="person-details card">
-            <img className="person-image"
-                src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+export default class PersonDetails extends React.Component {
 
-            <div className="card-body">
-                <h4>R2-D2</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">
-                        <span className="term">Gender</span>
-                        <span>male</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="term">Birth Year</span>
-                        <span>43</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="term">Eye Color</span>
-                        <span>red</span>
-                    </li>
-                </ul>
+    constructor(props){
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div className="person-details card">
+                <img className="person-image"
+                    src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+
+                <div className="card-body">
+                    <h4>R2-D2</h4>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">
+                            <span className="term">Gender</span>
+                            <span>male</span>
+                        </li>
+                        <li className="list-group-item">
+                            <span className="term">Birth Year</span>
+                            <span>43</span>
+                        </li>
+                        <li className="list-group-item">
+                            <span className="term">Eye Color</span>
+                            <span>red</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
-
-export default PersonDetails;
