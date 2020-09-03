80285912034ce2865eef717767c0a01d29692b31
revuwem <revuwem@gmail.com>
2020-09-03 12:05:54 +0300

    created rendering of people list

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,21 +1,56 @@
 import React from 'react';
 
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
+
 import './item-list.css';
+
 
 export default class ItemList extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            peopleList: null
+        }
+    }
+
+    componentDidMount() {
+        this.swapiService = new SwapiService();
+        this.swapiService
+            .getAllPeople()
+            .then((peopleList) => {
+                this.setState({
+                    peopleList
+                });
+            });
+    }
+
+    renderItems(arr) {
+        return arr.map(({ id, name }) => {
+            return (
+                <li className="list-group-item"
+                    key={id}
+                    onClick={()=>this.props.onItemSelected(id)}>
+                    {name}
+                </li>
+            );
+        })
+    }
+
     render() {
+
+        const { peopleList } = this.state;
+        if (!peopleList) {
+            return <Spinner />;
+        }
+
+        const items = this.renderItems(peopleList);
+
         return (
             <ul className="item-list list-group">
-                <li className="list-group-item">
-                    Luke Skywalker
-                </li>
-                <li className="list-group-item">
-                    Darth Vader
-                </li>
-                <li className="list-group-item">
-                    R2-D2
-                </li>
+                {items}
             </ul>
         );
     }
