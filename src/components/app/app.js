bd0f2cd3598c2352a40cb734982ed268066b74ec
revuwem <revuwem@gmail.com>
2020-09-03 12:09:38 +0300

    refactored to a class component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,22 +7,31 @@ import PersonDetails from '../person-details';
 
 import './app.css';
 
-const App = () => {
-    return(
-        <div>
-            <Header />
-            <RandomPlanet />
+export default class App extends React.Component {
 
-            <div className = "row mb-2">
-                <div className="col-md-6">
-                    <ItemList />
-                </div>
-                <div className="col-md-6">
-                    <PersonDetails />
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+   
+    render() {
+        return (
+            <div>
+                <Header />
+                <RandomPlanet />
+
+                <div className="row mb-2">
+                    <div className="col-md-6">
+                        <ItemList />
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
-
-export default App;
