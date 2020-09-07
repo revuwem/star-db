323c40a9bc5bf7a245c21cf37dc418448186e4f8
revuwem <revuwem@gmail.com>
2020-09-07 17:34:35 +0300

    components extracted to own files

---
+++ src/components/sw-components/person-details.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import ItemDetails, { Record } from '../item-details';
+import { SwapiServiceConsumer } from '../swapi-service-context';
+
+const PersonDetails = ({ itemId }) => {
+    return (
+        <SwapiServiceConsumer>
+            {
+                ({ getPerson, getPersonImage }) => {
+                    return (
+                        <ItemDetails
+                            itemId={itemId}
+                            getData={getPerson}
+                            getImageUrl={getPersonImage}>
+
+                            <Record field="gender" label="Gender" />
+                            <Record field="birthYear" label="Birth Year" />
+                            <Record field="eyeColor" label="Eye Color" />
+
+                        </ItemDetails>
+                    );
+                }
+            }
+        </SwapiServiceConsumer>
+    );
+};
+
+
+
+export default PersonDetails;
