44f7fc774026a0f4bec1a2aab5163afb6c6da4b8
revuwem <revuwem@gmail.com>
2020-09-11 11:56:56 +0300

    create props type validation

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
@@ -10,6 +11,10 @@ export default class RandomPlanet extends React.Component {
 
     static defaultProps = {
         updateInterval: 5000
+    };
+
+    static propTypes={
+        updateInterval: PropTypes.number
     };
 
     constructor(props) {
